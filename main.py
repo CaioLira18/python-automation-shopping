e1f5e86de1aa2250a7372dfbbeebbfd0a8d7a874
CaioLira18 <caiozao1212@gmail.com>
2025-11-12 23:26:30 -0300

    [feat] - Calculando o Faturamento

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ import pandas as pd
 tabelas_vendas = pd.read_excel('Vendas.xlsx')
 
 # Visualizar a base de dados
+pd.set_option('display.max_columns', None)
+print(tabelas_vendas)
 
 # Calcular o Faturamento
+faturamento = tabelas_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+print(faturamento)
+
 # Qtd de Produtos vendidos por loja
+
 # Ticket médio por produto em cada loja
 # Enviar um email com o relatorio
