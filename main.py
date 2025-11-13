db05c852273c297e4696e70a56e5875859af8032
CaioLira18 <caiozao1212@gmail.com>
2025-11-12 23:07:20 -0300

    [feat] - importing libraries and reading xlsx

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Importar a base de dados
+tabelas_vendas = pd.read_excel('Vendas.xlsx')
+
+# Visualizar a base de dados
+
+# Calcular o Faturamento
+# Qtd de Produtos vendidos por loja
+# Ticket médio por produto em cada loja
+# Enviar um email com o relatorio
