b23f4c6149b0897f64d7f93053b1b7010e55545e
CaioLira18 <caiozao1212@gmail.com>
2025-11-12 23:38:25 -0300

    [feat] - Calculando Quantidade e Ticket Medio

--- main.py
+++ main.py
@@ -7,11 +7,19 @@ tabelas_vendas = pd.read_excel('Vendas.xlsx')
 pd.set_option('display.max_columns', None)
 print(tabelas_vendas)
 
+print('-' * 50)
 # Calcular o Faturamento
 faturamento = tabelas_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
 print(faturamento)
 
+print('-' * 50)
 # Qtd de Produtos vendidos por loja
+quantidade = tabelas_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+print(quantidade)
 
+print('-' * 50)
 # Ticket médio por produto em cada loja
+ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
+print(ticket_medio)
+
 # Enviar um email com o relatorio
